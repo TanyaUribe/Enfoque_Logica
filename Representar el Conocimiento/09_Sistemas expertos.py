475f097d701b56f8031e5ea32acdf541894c127a
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-28 03:00:05 -0600

    Create 09_Sistemas expertos.py

---
+++ Representar el Conocimiento/09_Sistemas expertos.py
@@ -0,0 +1,34 @@
+class SistemaExperto:
+    def __init__(self):
+        self.base_conocimiento = {
+            "Cielo nublado": {"Actividad al aire libre": 0.6},
+            "Viento fuerte": {"Actividad al aire libre": 0.4},
+        }
+
+    def razonar(self, condiciones):
+        recomendaciones = {}
+        for condicion, valor in condiciones.items():
+            if condicion in self.base_conocimiento:
+                for actividad, factor in self.base_conocimiento[condicion].items():
+                    if actividad not in recomendaciones:
+                        recomendaciones[actividad] = 1.0
+                    recomendaciones[actividad] *= factor if valor else 1 - factor
+        return recomendaciones
+
+# Crear un sistema experto
+sistema = SistemaExperto()
+
+# Definir las condiciones meteorológicas
+condiciones = {
+    "Cielo nublado": True,
+    "Viento fuerte": False
+}
+
+# Realizar el razonamiento
+recomendaciones = sistema.razonar(condiciones)
+
+# Mostrar las recomendaciones
+print("Recomendaciones de actividades:")
+for actividad, certeza in recomendaciones.items():
+    if certeza >= 0.5:
+        print(f"{actividad} (Certidumbre: {certeza})")
