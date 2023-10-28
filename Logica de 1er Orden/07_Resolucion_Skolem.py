5c1089f509aaacdfaba3d780cf332a1626a0d3c3
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-28 02:46:11 -0600

    Update 07_Resolucion_Skolem.py

--- Logica de 1er Orden/07_Resolucion_Skolem.py
+++ Logica de 1er Orden/07_Resolucion_Skolem.py
@@ -1,36 +1,16 @@
-from sympy import symbols, Not, And, Or, Implies, Equivalent
 
-# Definimos las variables simbólicas
-p, q = symbols('p q')
+from sympy import symbols, Not, Or, simplify
 
-# Creamos expresiones lógicas
-expresion1 = And(p, q)  # p AND q
-expresion2 = Or(p, q)   # p OR q
-expresion3 = Implies(p, q)  # p IMPLIES q
-expresion4 = Equivalent(p, q)  # p IF AND ONLY IF q
+# Definición de variables
+x, y = symbols('x y')
 
-# Imprimimos las expresiones lógicas
-print("Expresión 1 (p AND q):", expresion1)
-print("Expresión 2 (p OR q):", expresion2)
-print("Expresión 3 (p IMPLIES q):", expresion3)
-print("Expresión 4 (p IF AND ONLY IF q):", expresion4)
+# Expresión con cuantificador existencial
+existencial_expr = Or(x > 0, Or(Not(y < 0), Not(y > 5)))
 
-# Evaluamos las expresiones para algunos valores de p y q
-valores = [(True, True), (True, False), (False, True), (False, False)]
+# Skolemización (aproximación)
+skolem_expr = simplify(existencial_expr)
 
-for p_valor, q_valor in valores:
-    resultado1 = expresion1.subs({p: p_valor, q: q_valor})
-    resultado2 = expresion2.subs({p: p_valor, q: q_valor})
-    resultado3 = expresion3.subs({p: p_valor, q: q_valor})
-    resultado4 = expresion4.subs({p: p_valor, q: q_valor})
-
-    print(f"Para p={p_valor}, q={q_valor}:")
-    print(f"Expresión 1 (p AND q) = {resultado1}")
-    print(f"Expresión 2 (p OR q) = {resultado2}")
-    print(f"Expresión 3 (p IMPLIES q) = {resultado3}")
-    print(f"Expresión 4 (p IF AND ONLY IF q) = {resultado4}")
-    print()
-
-# También podemos aplicar negación a una expresión lógica
-negacion_expresion1 = Not(expresion1)
-print("Negación de la Expresión 1 (NOT p AND q):", negacion_expresion1)
+print("Expresión original con cuantificador existencial:")
+print(existencial_expr)
+print("Expresión skolemizada (aproximada):")
+print(skolem_expr)
