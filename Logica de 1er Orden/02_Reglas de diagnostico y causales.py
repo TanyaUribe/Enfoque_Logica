8f69cd63ca927b6226232b1331e20add3fdebfe7
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-28 02:37:10 -0600

    Create 02_Reglas de diagnostico y causales.py

---
+++ Logica de 1er Orden/02_Reglas de diagnostico y causales.py
@@ -0,0 +1,15 @@
+from sympy import symbols, Not, Or, simplify
+
+# Definición de variables
+x, y = symbols('x y')
+
+# Expresión con cuantificador existencial
+existencial_expr = Or(x > 0, Or(Not(y < 0), Not(y > 5)))
+
+# Skolemización (aproximación)
+skolem_expr = simplify(existencial_expr)
+
+print("Expresión original con cuantificador existencial:")
+print(existencial_expr)
+print("Expresión skolemizada (aproximada):")
+print(skolem_expr)
