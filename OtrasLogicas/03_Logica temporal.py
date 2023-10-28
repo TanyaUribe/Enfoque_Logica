d95a8c4f9d8588cee8e2b50410ccdd13cdede3cd
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-28 02:51:39 -0600

    Update 03_Logica temporal.py

--- OtrasLogicas/03_Logica temporal.py
+++ OtrasLogicas/03_Logica temporal.py
@@ -1,24 +1,59 @@
-from datetime import datetime
+"""
+La lógica temporal es una herramienta importante en la inteligencia artificial, 
+especialmente en la representación del conocimiento y la planificación
+de agentes racionales que interactúan con un entorno a lo largo del tiempo
+Primero, se debe instalar la biblioteca "pddlpy" utilizando pip:
+pip install pddlpy
 
-class Alimento:
-    def __init__(self, nombre, fecha_vencimiento, es_vegetariano):
-        self.nombre = nombre
-        self.fecha_vencimiento = fecha_vencimiento
-        self.es_vegetariano = es_vegetariano
 
-    def se_puede_comer(self):
-        ahora = datetime.now()
-        if ahora < self.fecha_vencimiento and self.es_vegetariano:
-            return f"Se puede comer el alimento {self.nombre}."
-        elif ahora >= self.fecha_vencimiento:
-            return f"No se puede comer el alimento {self.nombre} porque ha vencido."
-        else:
-            return f"No se puede comer el alimento {self.nombre}."
 
-# Crear un alimento con fecha de vencimiento
-fecha_vencimiento_ensalada = datetime(2023, 12, 31)
-ensalada = Alimento("ensalada", fecha_vencimiento_ensalada, es_vegetariano=True)
+Supongamos que queremos planificar un viaje en coche desde una ciudad A a una ciudad B. 
+El viaje debe incluir cargar combustible en una gasolinera 
+(G) en algún momento antes de llegar a B. Definiremos esta tarea como un problema de planificación en lógica temporal.
+El planificador encuentra una secuencia de acciones que satisfacen los objetivos definidos en el problema.
+"""
 
-# Realizar una consulta para saber si se puede comer
-decision = ensalada.se_puede_comer()
-print(decision)
+from pddlpy import DomainProblem, state_from_fact, Action, Domain
+
+# Define el dominio
+domain = Domain((
+    # Predicados
+    ('at', 'car', '?from', '?to'),
+    ('at', 'gas_station', '?loc'),
+    ('has_fuel', 'car'),
+    ('route', '?from', '?to'),
+
+    # Acciones
+    Action('drive', parameters=(('?from', 'loc'), ('?to', 'loc')),
+           condition=(('at', 'car', '?from'), ('route', '?from', '?to'), ('has_fuel', 'car')),
+           effect=(('at', 'car', '?to'), ('not', ('at', 'car', '?from')))),
+
+    Action('refuel', parameters=(('?loc', 'loc')),
+           condition=(('at', 'car', '?loc'), ('at', 'gas_station', '?loc')),
+           effect=(('has_fuel', 'car'),)),
+))
+
+# Define el problema
+problem = domain.problem(
+    "car_problem",
+    objects=('car', 'gas_station', 'cityA', 'cityB'),
+    initial=(
+        ('at', 'car', 'cityA'),
+        ('at', 'gas_station', 'cityA'),
+        ('route', 'cityA', 'cityB'),
+    ),
+    goal=(
+        ('at', 'car', 'cityB'),
+        ('has_fuel', 'car'),
+    )
+)
+
+# Encuentra un plan
+plan = problem.happenings(50)  # Encuentra un plan con un límite de 50 pasos
+
+if plan:
+    print("Plan encontrado:")
+    for action, args in plan:
+        print(f"- {action}({', '.join(args)})")
+else:
+    print("No se encontró un plan.")
